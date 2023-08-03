c5cde3fe89a5d609e144175fc66041ae2877ee2f
jecaro <jeancharles.quillet@gmail.com>
2023-08-03 09:52:26 +0200

    tutorial 1

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -5,14 +5,7 @@
 
 module Frontend where
 
-import Common.Api
 import Common.Route
-import Control.Lens ((^.))
-import Control.Monad
-import qualified Data.Text as T
-import qualified Data.Text.Encoding as T
-import Language.Javascript.JSaddle (js, js1, jsg, liftJSM)
-import Obelisk.Configs
 import Obelisk.Frontend
 import Obelisk.Generated.Static
 import Obelisk.Route
@@ -26,31 +19,24 @@ frontend =
   Frontend
     { _frontend_head = do
         el "title" $ text "Obelisk Minimal Example"
-        elAttr "script" ("type" =: "application/javascript" <> "src" =: $(static "lib.js")) blank
-        elAttr "link" ("href" =: $(static "main.css") <> "type" =: "text/css" <> "rel" =: "stylesheet") blank,
+        elAttr
+          "script"
+          ( "type" =: "application/javascript"
+              <> "src" =: $(static "lib.js")
+          )
+          blank
+        elAttr
+          "link"
+          ( "href" =: $(static "main.css")
+              <> "type" =: "text/css"
+              <> "rel" =: "stylesheet"
+          )
+          blank,
       _frontend_body = do
         el "h1" $ text "Welcome to Obelisk!"
-        el "p" $ text $ T.pack commonStuff
 
-        -- `prerender` and `prerender_` let you choose a widget to run on the server
-        -- during prerendering and a different widget to run on the client with
-        -- JavaScript. The following will generate a `blank` widget on the server and
-        -- print "Hello, World!" on the client.
-        prerender_ blank $
-          liftJSM $
-            void $
-              jsg ("window" :: T.Text)
-                ^. js ("skeleton_lib" :: T.Text)
-                ^. js1 ("log" :: T.Text) ("Hello, World!" :: T.Text)
-
-        elAttr "img" ("src" =: $(static "obelisk.jpg")) blank
-        el "div" $ do
-          let cfg = "common/example"
-              path = "config/" <> cfg
-          getConfig cfg >>= \case
-            Nothing -> text $ "No config file found in " <> path
-            Just bytes -> case T.decodeUtf8' bytes of
-              Left ue -> text $ "Couldn't decode " <> path <> " : " <> T.pack (show ue)
-              Right s -> text s
-        return ()
+        tutorial1
     }
+
+tutorial1 :: DomBuilder t m => m ()
+tutorial1 = el "div" $ text "Welcome to Reflex"
