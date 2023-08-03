817b1434a4096ac11a859162fc938599a808e0ea
jecaro <jeancharles.quillet@gmail.com>
2023-08-03 10:31:26 +0200

    tutorial 7

--- frontend/src/Frontend.hs
+++ frontend/src/Frontend.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -6,7 +7,10 @@
 module Frontend where
 
 import Common.Route
-import Data.Text (pack, unpack)
+import Control.Monad.Fix
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
+import Data.Text (Text, pack, unpack)
 import Obelisk.Frontend
 import Obelisk.Generated.Static
 import Obelisk.Route
@@ -48,6 +52,8 @@ frontend =
         tutorial5
         el "hr" blank
         tutorial6
+        el "hr" blank
+        tutorial7
     }
 
 tutorial1 :: DomBuilder t m => m ()
@@ -104,6 +110,39 @@ tutorial6 = el "div" $ do
   let result = zipDynWith (\x y -> (+) <$> x <*> y) nx ny
       resultString = fmap (pack . show) result
   dynText resultString
+  where
+    numberInput :: DomBuilder t m => m (Dynamic t (Maybe Double))
+    numberInput = do
+      n <-
+        inputElement $
+          def
+            & inputElementConfig_initialValue .~ "0"
+            & inputElementConfig_elementConfig . elementConfig_initialAttributes
+              .~ ("type" =: "number")
+      return . fmap (readMaybe . unpack) $ _inputElement_value n
+
+data Op = Plus | Minus | Times | Divide
+  deriving stock (Eq, Ord, Show)
+
+runOp :: Fractional a => Op -> a -> a -> a
+runOp Plus = (+)
+runOp Minus = (-)
+runOp Times = (*)
+runOp Divide = (/)
+
+ops :: Map Op Text
+ops = Map.fromList [(Plus, "+"), (Minus, "-"), (Times, "*"), (Divide, "/")]
+
+tutorial7 :: (DomBuilder t m, PostBuild t m, MonadHold t m, MonadFix m) => m ()
+tutorial7 = el "div" $ do
+  nx <- numberInput
+  op <- _dropdown_value <$> dropdown Times (constDyn ops) def
+  ny <- numberInput
+  let values = zipDynWith (,) nx ny
+      result = zipDynWith (\o (x, y) -> runOp o <$> x <*> y) op values
+      resultText = fmap (pack . show) result
+  text " = "
+  dynText resultText
   where
     numberInput :: DomBuilder t m => m (Dynamic t (Maybe Double))
     numberInput = do
